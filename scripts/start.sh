3f4a5f9518f5ae2fd83006ca38ddadb9c3cf9102
Christoph <christophhummer@gmail.com>
2022-04-12 13:29:49 +0200

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -27,11 +27,6 @@ chown -R root:${GID} /opt/scripts
 chmod -R 750 /opt/scripts
 chown -R ${UID}:${GID} ${DATA_DIR}
 
-echo "---Starting...---"
-chown -R root:${GID} /opt/scripts
-chmod -R 750 /opt/scripts
-chown -R ${UID}:${GID} ${DATA_DIR}
-
 term_handler() {
 	kill -SIGTERM "$killpid"
 	wait "$killpid" -f 2>/dev/null
